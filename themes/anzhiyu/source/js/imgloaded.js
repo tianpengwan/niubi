3d4a72fbe2b590b51b94e4fac76ad8d64b05cfb1
tianpengwan <102905945+tianpengwan@users.noreply.github.com>
2026-06-20 03:22:55 +0800

    Update and rename simgloaded.js to imgloaded.js

--- themes/anzhiyu/source/js/imgloaded.js
+++ themes/anzhiyu/source/js/imgloaded.js
@@ -59,17 +59,17 @@
   
     const ldconfig = {
       light: {
-        smallSrc: 'https://lsky.mnchen.cn/i/2023/10/24/6536faa024dfa.jpg', //浅色模式 小图链接 尽可能配置小于100k的图片 
-        largeSrc: 'https://lsky.mnchen.cn/i/2023/10/24/6536faa024dfa.jpg', //浅色模式 大图链接 最终显示的图片
-        mobileSmallSrc: 'https://lsky.mnchen.cn/i/2023/10/24/6536faa024dfa.jpg', //手机端浅色小图链接 尽可能配置小于100k的图片
-        mobileLargeSrc: 'https://lsky.mnchen.cn/i/2023/10/24/6536faa024dfa.jpg', //手机端浅色大图链接 最终显示的图片
+        smallSrc: 'https://t.alcy.cc/pc/', //浅色模式 小图链接 尽可能配置小于100k的图片
+        largeSrc: 'https://t.alcy.cc/pc/', //浅色模式 大图链接 最终显示的图片
+        mobileSmallSrc: 'https://t.alcy.cc/pc/', //手机端浅色小图链接 尽可能配置小于100k的图片
+        mobileLargeSrc: 'https://t.alcy.cc/pc/', //手机端浅色大图链接 最终显示的图片
         enableRoutes: ['/'],
         },
       dark: {
-        smallSrc: 'https://lsky.mnchen.cn/i/2023/11/10/654e3e6d29b3f.png', //深色模式 小图链接 尽可能配置小于100k的图片 
-        largeSrc: 'https://lsky.mnchen.cn/i/2023/11/10/654e3e6d29b3f.png', //深色模式 大图链接 最终显示的图片
-        mobileSmallSrc: 'https://lsky.mnchen.cn/i/2023/11/10/654e3e6d29b3f.png', //手机端深色模式小图链接 尽可能配置小于100k的图片
-        mobileLargeSrc: 'https://lsky.mnchen.cn/i/2023/11/10/654e3e6d29b3f.png', //手机端深色大图链接 最终显示的图片
+        smallSrc: 'https://t.alcy.cc/pc/', //深色模式 小图链接 尽可能配置小于100k的图片 
+        largeSrc: 'https://t.alcy.cc/pc/', //深色模式 大图链接 最终显示的图片
+        mobileSmallSrc: 'https://t.alcy.cc/pc/', //手机端深色模式小图链接 尽可能配置小于100k的图片
+        mobileLargeSrc: 'https://t.alcy.cc/pc/', //手机端深色大图链接 最终显示的图片
         enableRoutes: ['/'],
         },
       };
